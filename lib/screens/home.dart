5074766b67ad74037c44c6c5fee7bc3e3c9dd5ea
Komang Ariana <km.ariana1992@gmail.com>
2020-04-26 07:55:28 +0800

    Create function to display greetings and time.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,11 +7,29 @@ class Home extends StatelessWidget {
       color: Colors.blue,
       child: Center(
         child: new Text(
-          "Hello World!",
+          sayHello(),
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 32)
         ),
       ),
     );
+  }
+  String sayHello() {
+    String hello;
+    DateTime now = new DateTime.now();
+    int hour = now.hour;
+    int minute = now.minute;
+
+    if (hour < 12) {
+      hello = "Good Morning Good People";
+    } else if (hour < 18) {
+      hello = "Good Afternoon Good People";
+    } else {
+      hello = "Good Evening Good People";
+    }
+
+    String minutes = (minute < 10) ? "0" + minute.toString() : minute.toString();
+
+    return "Now is " + hour.toString() + ":" + minutes + " o'clock" + "\n" + hello;
   }
 }
