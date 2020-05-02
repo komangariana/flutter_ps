0e97f61fe1a014c1b9009b8cd31ab334ef271729
Komang Ariana <km.ariana1992@gmail.com>
2020-05-02 16:46:03 +0800

    feat: Init main dart and import home UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
-import './screens/home.dart';
+import './ui/home.dart';
 
-void main() => runApp(new HelloWorldApp());
-
-class HelloWorldApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Flutter App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Title App Bar"),
-        ),
-        body: Home()
-      ),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Simple Layouts",
+    home: new Home(),
+  ));
 }
