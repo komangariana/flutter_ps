b2f1b184dc7619c8f85c6ea06e894595192912a8
Komang Ariana <km.ariana1992@gmail.com>
2020-05-07 16:38:27 +0800

    create list view of todolist.

---
+++ flutter_todo_app/lib/screens/todolist.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/util/dbhelper.dart';
+import 'package:flutter_todo_app/model/todo.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => TodoListState();
+}
+
+class TodoListState extends State {
+  DbHelper helper = DbHelper();
+  List<Todo> todos;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (todos == null) {
+      todos = List<Todo>();
+      getData();
+    }
+    return Scaffold(
+      body: todoListItems(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: "Add New Todo List",
+        child: Icon(Icons.add)
+      )
+    );
+  }
+
+  ListView todoListItems() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Text(this.todos[position].id.toString())
+            ),
+            title: Text(this.todos[position].title),
+            subtitle: Text(this.todos[position].date),
+            onTap: () {
+              debugPrint(this.todos[position].id.toString());
+            }
+          ),
+        );
+      }
+    );
+  }
+
+  void getData() {
+    final dbFuture = helper.initializeDb();
+    dbFuture.then((result) {
+      final todosFuture = helper.getTodos();
+      todosFuture.then((result) {
+        List<Todo> todoList = List<Todo>();
+        count = result.length;
+        for (int i=0; i<count; i++) {
+          todoList.add(Todo.fromObject(result[i]));
+          debugPrint(todoList[i].title);
+        }
+        setState(() {
+          todos = todoList;
+          count = count;
+        });
+        debugPrint("Items " + count.toString());
+      });
+    });
+  }
+}
