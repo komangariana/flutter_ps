e7d96289dee10cdd027a0e29a0db6ef4dc3f4c67
Komang Ariana <km.ariana1992@gmail.com>
2020-05-10 08:55:50 +0800

    Add padding to detail page.

--- flutter_todo_app/lib/screens/tododetail.dart
+++ flutter_todo_app/lib/screens/tododetail.dart
@@ -31,44 +31,49 @@ class TodoDetailState extends State {
         automaticallyImplyLeading: false,
         title: Text(todo.title),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            controller: titleController,
-            style: textStyle,
-            decoration: InputDecoration(
-              labelText: "Title",
-              labelStyle: textStyle,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0)
-              )
+      body: Padding(
+        padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              style: textStyle,
+              decoration: InputDecoration(
+                labelText: "Title",
+                labelStyle: textStyle,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+                )
+              ),
             ),
-          ),
-          TextField(
-            controller: descriptionController,
-            style: textStyle,
-            decoration: InputDecoration(
-              labelText: "Description",
-              labelStyle: textStyle,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0)
-              )
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: TextField(
+                controller: descriptionController,
+                style: textStyle,
+                decoration: InputDecoration(
+                  labelText: "Description",
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                  )
+                ),
+              ),
             ),
-          ),
-          DropdownButton<String>(
-            items: _priorities.map((String value) {
-              return DropdownMenuItem<String> (
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            style: textStyle,
-            value: "Low",
-            onChanged: null,
-          ),
-        ],
+            DropdownButton<String>(
+              items: _priorities.map((String value) {
+                return DropdownMenuItem<String> (
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              style: textStyle,
+              value: "Low",
+              onChanged: null,
+            ),
+          ],
+        )
       )
-
     );
   }
 }
