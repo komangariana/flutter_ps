e20d1a5bceffa94c6ce54e15948b93ee99c74cf1
Komang Ariana <km.ariana1992@gmail.com>
2020-05-10 10:14:50 +0800

    get result data to todolist page.

--- flutter_todo_app/lib/screens/todolist.dart
+++ flutter_todo_app/lib/screens/todolist.dart
@@ -95,5 +95,8 @@ class TodoListState extends State {
     bool result = await Navigator.push(context,
       MaterialPageRoute(builder: (context) => TodoDetail(todo)),
     );
+    if (result == true) {
+      getData();
+    }
   }
 }
