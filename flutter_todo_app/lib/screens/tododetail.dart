3ac3722b36d74f6c593598ceb3c32b1966450286
Komang Ariana <km.ariana1992@gmail.com>
2020-05-10 10:14:37 +0800

    create save, delete and back functionality.

--- flutter_todo_app/lib/screens/tododetail.dart
+++ flutter_todo_app/lib/screens/tododetail.dart
@@ -3,6 +3,18 @@ import 'package:flutter_todo_app/util/dbhelper.dart';
 import 'package:flutter_todo_app/model/todo.dart';
 import 'package:intl/intl.dart';
 
+DbHelper helper = DbHelper(); 
+
+final List<String> choices = const <String> [
+  'Save Todo & Back',
+  'Delete Todo',
+  'Back to List'
+];
+
+const menuSave = 'Save Todo & Back';
+const menuDelete = 'Delete Todo';
+const menuBack = 'Back to List';
+
 class TodoDetail extends StatefulWidget {
   final Todo todo;
   TodoDetail(this.todo);
@@ -30,6 +42,19 @@ class TodoDetailState extends State {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text(todo.title),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: select,
+            itemBuilder: (BuildContext context) {
+              return choices.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            }
+          )
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
@@ -40,6 +65,7 @@ class TodoDetailState extends State {
                 TextField(
                   controller: titleController,
                   style: textStyle,
+                  onChanged: (value) => this.updateTitle(),
                   decoration: InputDecoration(
                     labelText: "Title",
                     labelStyle: textStyle,
@@ -53,6 +79,7 @@ class TodoDetailState extends State {
                   child: TextField(
                     controller: descriptionController,
                     style: textStyle,
+                    onChanged: (value) => this.updateDescription(),
                     decoration: InputDecoration(
                       labelText: "Description",
                       labelStyle: textStyle,
@@ -71,8 +98,9 @@ class TodoDetailState extends State {
                       );
                     }).toList(),
                     style: textStyle,
-                    value: _priority,
+                    value: retrievePriority(todo.priority),
                     onChanged: (String value) {
+                      translatePriority(value);
                       debugPrint(value );
                     },
                   ),
@@ -83,5 +111,75 @@ class TodoDetailState extends State {
         ),
       )
     );
+  }
+
+  void select (String value) async {
+    int result;
+    switch (value) {
+      case menuSave:
+        save();
+        break;
+      case menuDelete:
+        Navigator.pop(context, true);
+        if (todo.id == null) {
+          return;
+        }
+        result = await helper.deleteTodo(todo.id);
+        if (result != 0) {
+          AlertDialog alertDialog = AlertDialog(
+            title: Text("Delete"),
+            content: Text("Todo has been deleted!")
+          );
+          showDialog(
+            context: context,
+            builder: (_) => alertDialog,
+          );
+        }
+        break;
+      case menuBack: 
+        Navigator.pop(context, true);
+        break;
+      default:
+    }
+  }
+
+  void save () {
+    todo.date = new DateFormat.yMd().format(DateTime.now());
+    if (todo.id != null) {
+      helper.updateTodo(todo);
+    } else {
+      helper.insertTodo(todo);
+    }
+    Navigator.pop(context, true);
+  }
+
+  void translatePriority (String value) {
+    switch (value) {
+      case "High":
+        todo.priority = 1;
+        break;
+      case "Medium":
+        todo.priority = 2;
+        break;
+      case "Low":
+        todo.priority = 3;
+        break;
+
+      setState(() {
+        _priority = value;
+      });
+    }
+  }
+
+  String retrievePriority(int value) {
+    return _priorities[value-1];
+  }
+
+  void updateTitle() {
+    todo.title = titleController.text;
+  }
+
+  void updateDescription() {
+    todo.description = descriptionController.text;
   }
 }
