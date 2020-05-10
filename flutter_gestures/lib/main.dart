8a6c8de7fb984943fb57ed3791b0c52a4870a24f
Komang Ariana <km.ariana1992@gmail.com>
2020-05-10 12:52:40 +0800

    add drag vertical and horizontal gesture.

--- flutter_gestures/lib/main.dart
+++ flutter_gestures/lib/main.dart
@@ -53,6 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
             numLongPress++;
           });
         },
+        onVerticalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            double delta = value.delta.dy;
+            posY += delta;
+          });
+        },
+        onHorizontalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            double delta = value.delta.dx;
+            posX += delta;
+          });
+        },
         child: Stack(
           children: <Widget>[
             Positioned(
